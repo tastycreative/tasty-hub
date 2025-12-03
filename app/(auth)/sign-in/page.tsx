3452753b001c09ac504dd54922b4ebcb4437f762
kenjanjan <kjliloc@hcdc.edu.ph>
2025-12-03 09:11:22 +0800

    Refactor authentication forms to use AuthCard component for improved structure and consistency

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
-import { GalleryVerticalEnd } from "lucide-react"
+import { GalleryVerticalEnd } from "lucide-react";
 
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/auth/login-form";
 
 export default function SignInPage() {
   return (
@@ -15,5 +15,5 @@ export default function SignInPage() {
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { GalleryVerticalEnd } from "lucide-react";
 
-const page = () => {
+import { SignupForm } from "@/components/auth/signup-form";
+
+export default function SignUpPage() {
   return (
-    <div>page</div>
-  )
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Tasty Hub.
+        </a>
+        <SignupForm />
+      </div>
+    </div>
+  );
 }
-
-export default page
